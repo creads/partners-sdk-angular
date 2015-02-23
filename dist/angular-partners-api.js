7d18a9097c8bb7a5a59d13e9cbe5546d27fff414
Samuel Vaillant <s.vaillant@creads.org>
2015-02-23 12:35:00 +0100

    dump dist

--- dist/angular-partners-api.js
+++ dist/angular-partners-api.js
@@ -120,72 +120,119 @@
     .provider('apiInterceptor', function() {
 
       var apiInterceptor = {},
+          isAbleToCatchAllRequest = false,
           onRequest,
           onRequestError,
           onResponse,
           onResponseError;
 
+      /**
+       * Check if the given object is function
+       * @param  {object}  object
+       * @return {Boolean}
+       */
       function isFunction(object) {
         return object && {}.toString.call(object) === '[object Function]';
       }
 
       /**
-       * [setOnRequest description]
+       * Check if request need to be intercepted
+       * @param  {string}  pattern Pattern of the URL to intercept
+       * @param  {string}  url     URL to check
+       * @return {Boolean}
+       */
+      function isIntercept(pattern, url) {
+        return (isAbleToCatchAllRequest || new RegExp('^' + pattern).test(url)) ? true : false;
+      }
+
+      /**
+       * [setIsAbleToCatchAllRequest]
+       * @param {boolean} value
+       */
+      this.setIsAbleToCatchAllRequest = function setIsAbleToCatchAllRequest(value) {
+        if (typeof value !== 'boolean') {
+          throw new Error('Boolean value is provide for option IsAbleToCatchAllRequest');
+        }
+
+        isAbleToCatchAllRequest = value;
+
+        return this;
+      };
+
+      /**
+       * [setOnRequest]
        * @param {[type]} callbackRequest
        * @return this
        */
       this.setOnRequest = function setOnRequest(callbackRequest) {
+        if (!isFunction(callbackRequest)) {
+          throw new Error('Function is provide for option onRequest');
+        }
+
         onRequest = callbackRequest;
 
         return this;
       };
 
       /**
-       * [setOnRequestError description]
+       * [setOnRequestError]
        * @param {[type]} callbackRequestError
        * @return this
        */
       this.setOnRequestError = function setOnRequestError(callbackRequestError) {
+        if (!isFunction(callbackRequestError)) {
+          throw new Error('Function is provide for option onRequestError');
+        }
+
         onRequestError = callbackRequestError;
 
         return this;
       };
 
       /**
-       * [onResponse description]
+       * [setOnResponse]
        * @param  {[type]} callbackResponse
        * @return this
        */
-      this.onResponse = function setOnResponse(callbackResponse) {
+      this.setOnResponse = function setOnResponse(callbackResponse) {
+        if (!isFunction(callbackResponse)) {
+          throw new Error('Function is provide for option onResponse');
+        }
+
         onResponse = callbackResponse;
 
         return this;
       };
 
       /**
-       * [setOnResponseError description]
-       * @param {[type]} callbackRequest
+       * [setOnResponseError]
+       * @param {[type]} callbackResponseError
        * @return this
        */
       this.setOnResponseError = function setOnResponseError(callbackResponseError) {
+        if (!isFunction(callbackResponseError)) {
+          throw new Error('Function is provide for option onResponseError');
+        }
+
         onResponseError = callbackResponseError;
 
         return this;
       };
 
       this.$get = [
+        'PARTNERS_ENDPOINT',
         '$q',
         '$injector',
-        function apiInterceptor($q, $injector) {
+        function apiInterceptor(PARTNERS_ENDPOINT, $q, $injector) {
 
           /**
-           * [request description]
+           * [request]
            * @param  {[type]} config
            * @return {object}
            */
           apiInterceptor.request = function request(config) {
 
-            if (isFunction(onRequest)) {
+            if (isIntercept(PARTNERS_ENDPOINT, config.url) && onRequest !== undefined) {
               config = onRequest(config);
             }
 
@@ -193,13 +240,13 @@
           };
 
           /**
-           * [requestError description]
+           * [requestError]
            * @param  {object} rejection
            * @return {promise}
            */
           apiInterceptor.requestError = function requestError(rejection) {
 
-            if (isFunction(onRequestError)) {
+            if (isIntercept(PARTNERS_ENDPOINT, rejection.url) && onRequestError !== undefined) {
               onRequestError($injector, rejection);
             }
 
@@ -207,13 +254,13 @@
           };
 
           /**
-           * [response description]
+           * [response]
            * @param  {object} response
            * @return {object}
            */
           apiInterceptor.response = function response(_response) {
 
-            if (isFunction(onResponse)) {
+            if (isIntercept(PARTNERS_ENDPOINT, _response.config.url) && onResponse !== undefined) {
               _response = onResponse(_response);
             }
 
@@ -221,13 +268,13 @@
           };
 
           /**
-           * [responseError description]
+           * [responseError]
            * @param  {object} rejection
            * @return {promise}
            */
           apiInterceptor.responseError = function responseError(rejection) {
 
-            if (isFunction(onResponseError)) {
+            if (isIntercept(PARTNERS_ENDPOINT, rejection.config.url) && onResponseError !== undefined) {
               onResponseError($injector, rejection);
             }
 
