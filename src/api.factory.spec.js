d8b4fa63cd41bd75fb8029d2e39ac5e5659b3aae
Samuel Vaillant <s.vaillant@creads.org>
2015-03-16 11:15:45 +0100

    update faker variable for injector

--- src/api.factory.spec.js
+++ src/api.factory.spec.js
@@ -1,13 +1,13 @@
 describe('[Unit]: Testing api config provider', function() {
-  var apiProviderFaker;
+  var apiProvider;
 
   beforeEach(function() {
     angular
       .module('fakeModule', [])
       .config([
-        'apiProvider',
-        function(apiProvider) {
-          apiProviderFaker = apiProvider;
+        '$injector',
+        function($injector) {
+          apiProvider = $injector.get('apiProvider');
         }
       ])
     ;
@@ -19,15 +19,15 @@ describe('[Unit]: Testing api config provider', function() {
   describe('getEndpoint method', function() {
 
     it('should exist', function() {
-      expect(apiProviderFaker.getEndpoint).to.exist;
+      expect(apiProvider.getEndpoint).to.exist;
     });
 
     it('should be a function', function() {
-      expect(apiProviderFaker.getEndpoint).to.be.a('function');
+      expect(apiProvider.getEndpoint).to.be.a('function');
     });
 
     it('should return http://api.creads-partners.com', function() {
-      expect(apiProviderFaker.getEndpoint()).to.be.equal('http://api.creads-partners.com');
+      expect(apiProvider.getEndpoint()).to.be.equal('http://api.creads-partners.com');
     });
 
   });
@@ -35,26 +35,26 @@ describe('[Unit]: Testing api config provider', function() {
   describe('setEndpoint method', function() {
 
     it('should exist', function() {
-      expect(apiProviderFaker.setEndpoint).to.be.a('function');
+      expect(apiProvider.setEndpoint).to.be.a('function');
     });
 
     it('should throw an error if value is not string', function() {
-      expect(function() { apiProviderFaker.setEndpoint(); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setEndpoint(null); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setEndpoint(undefined); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setEndpoint({}); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setEndpoint([]); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setEndpoint(102); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setEndpoint(true); }).to.throw(Error);
+      expect(function() { apiProvider.setEndpoint(); }).to.throw(Error);
+      expect(function() { apiProvider.setEndpoint(null); }).to.throw(Error);
+      expect(function() { apiProvider.setEndpoint(undefined); }).to.throw(Error);
+      expect(function() { apiProvider.setEndpoint({}); }).to.throw(Error);
+      expect(function() { apiProvider.setEndpoint([]); }).to.throw(Error);
+      expect(function() { apiProvider.setEndpoint(102); }).to.throw(Error);
+      expect(function() { apiProvider.setEndpoint(true); }).to.throw(Error);
     });
 
     it('should set endpoint with https://api.github.com', function () {
-      apiProviderFaker.setEndpoint('https://api.github.com');
-      expect(apiProviderFaker.getEndpoint()).to.be.equal('https://api.github.com');
+      apiProvider.setEndpoint('https://api.github.com');
+      expect(apiProvider.getEndpoint()).to.be.equal('https://api.github.com');
     });
 
     it('should return an instance of apiProvider', function() {
-      expect(apiProviderFaker.setEndpoint('endpoint')).to.be.an.instanceof(apiProviderFaker.constructor);
+      expect(apiProvider.setEndpoint('endpoint')).to.be.an.instanceof(apiProvider.constructor);
     });
 
   });
@@ -62,15 +62,15 @@ describe('[Unit]: Testing api config provider', function() {
   describe('getVersion method', function() {
 
     it('should exist', function() {
-      expect(apiProviderFaker.getVersion).to.exist;
+      expect(apiProvider.getVersion).to.exist;
     });
 
     it('should be a function', function() {
-      expect(apiProviderFaker.getVersion).to.be.a('function');
+      expect(apiProvider.getVersion).to.be.a('function');
     });
 
     it('should return 0.0.0', function() {
-      expect(apiProviderFaker.getVersion()).to.be.equal('0.0.0');
+      expect(apiProvider.getVersion()).to.be.equal('0.0.0');
     });
 
   });
@@ -78,26 +78,26 @@ describe('[Unit]: Testing api config provider', function() {
   describe('setVersion method', function() {
 
     it('should exist', function() {
-      expect(apiProviderFaker.setVersion).to.be.a('function');
+      expect(apiProvider.setVersion).to.be.a('function');
     });
 
     it('should throw an error if value is not string', function() {
-      expect(function() { apiProviderFaker.setVersion(); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setVersion(null); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setVersion(undefined); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setVersion({}); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setVersion([]); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setVersion(102); }).to.throw(Error);
-      expect(function() { apiProviderFaker.setVersion(true); }).to.throw(Error);
+      expect(function() { apiProvider.setVersion(); }).to.throw(Error);
+      expect(function() { apiProvider.setVersion(null); }).to.throw(Error);
+      expect(function() { apiProvider.setVersion(undefined); }).to.throw(Error);
+      expect(function() { apiProvider.setVersion({}); }).to.throw(Error);
+      expect(function() { apiProvider.setVersion([]); }).to.throw(Error);
+      expect(function() { apiProvider.setVersion(102); }).to.throw(Error);
+      expect(function() { apiProvider.setVersion(true); }).to.throw(Error);
     });
 
     it('should set endpoint with https://api.github.com', function () {
-      apiProviderFaker.setVersion('https://api.github.com');
-      expect(apiProviderFaker.getVersion()).to.be.equal('https://api.github.com');
+      apiProvider.setVersion('https://api.github.com');
+      expect(apiProvider.getVersion()).to.be.equal('https://api.github.com');
     });
 
     it('should return an instance of apiProvider', function() {
-      expect(apiProviderFaker.setVersion('endpoint')).to.be.an.instanceof(apiProviderFaker.constructor);
+      expect(apiProvider.setVersion('endpoint')).to.be.an.instanceof(apiProvider.constructor);
     });
 
   });
@@ -106,22 +106,22 @@ describe('[Unit]: Testing api config provider', function() {
 
 
 describe('[Unit]: Testing api factory', function() {
-  var apiFaker;
+  var api;
 
   beforeEach(module('partners.api'));
 
-  beforeEach(inject(function(api) {
-    apiFaker = api;
+  beforeEach(inject(function($injector) {
+    api = $injector.get('api');
   }));
 
   describe('call method', function() {
 
     it('should exist', function() {
-      expect(apiFaker.call).to.exist;
+      expect(api.call).to.exist;
     });
 
     it('should be a function', function() {
-      expect(apiFaker.call).to.be.a('function');
+      expect(api.call).to.be.a('function');
     });
 
   });
