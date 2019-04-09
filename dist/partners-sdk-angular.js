781be92792d0897cea32e00e1ae4afbe22a97f59
Alex Stef <stefanski.alexis@gmail.com>
2019-04-09 17:44:19 +0200

    Bumped lib to 2.1.0 and apps to 100.platinum

--- dist/partners-sdk-angular.js
+++ dist/partners-sdk-angular.js
@@ -1,6 +1,6 @@
 /**
  * partners-sdk-angular - Angular module for call Creads partners API
- * @version v2.0.0
+ * @version v2.1.0
  * @link https://github.com/creads/partners-sdk-angular
  * @license proprietary
  */
